9045da41bd16663c35006fd5b5451273dde9744b
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-16 12:32:34 -0300

    Config ansible-pull git remote

--- scripts/userdata-grafana-stage.sh
+++ scripts/userdata-grafana-stage.sh
@@ -8,6 +8,7 @@ sudo apt install awscli aptitude git ansible python-pip -y
 sudo mkdir /data
 sudo echo "UUID=fd8202c6-f3fc-48ed-b71f-72647017a029  /data  xfs  defaults,nofail  0  2" >> /etc/fstab
 
-# sudo ansible-pull -U git@git..... --accept-host-key ./ansible/grafana.yml
+ansible-pull -U https://nttdatada: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@github.com/nttdatada/da-kickoff-devops-sre.git//ansible grafana.yml
+
 
 sudo reboot
