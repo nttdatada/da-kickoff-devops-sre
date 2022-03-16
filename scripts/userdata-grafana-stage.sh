cd6a873b597ddefd5523739c1afd3748adf0f435
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-16 11:31:01 -0300

    Userdata grafana mount /data in partition ebs

--- scripts/userdata-grafana-stage.sh
+++ scripts/userdata-grafana-stage.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt upgrade -y
+
 sudo apt install awscli aptitude git ansible python-pip -y
 
+sudo mkdir /data
+sudo echo "UUID=fd8202c6-f3fc-48ed-b71f-72647017a029  /data  xfs  defaults,nofail  0  2" >> /etc/fstab
+
 # sudo ansible-pull -U git@git..... --accept-host-key ./ansible/grafana.yml
+
+sudo reboot
