1f8f3b7f4a0367084f3d03d69ae3c59dbd40ee2b
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-16 13:01:38 -0300

    Set securtity group grafana in ec2

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "ec2_grafana" {
     iam_instance_profile    = module.instance_profile_ec2_grafana.instance_profile_name
     ami                     = data.aws_ami.ubuntu.id
     monitoring              = true
-    vpc_security_group_ids  = ["sg-0cb0f7f9f49b0876d"]
+    vpc_security_group_ids  = [ module.sg_grafana.id ]
     subnet_id               = "subnet-2eb65362"
 
     user_data               = data.template_file.user_data_grafana.rendered
